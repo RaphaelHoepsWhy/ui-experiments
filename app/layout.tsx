31ee137e4e0cefc46c53f0bc5a7f8d2d9eb2603d
RaphaelHoepsWhy <raphael.hoeps@why.de>
2025-07-15 14:37:17 +0200

    feat: checkbox base setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} dark font-sans antialiased`}
       >
         <div className="p-8">{children}</div>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+import { Checkbox } from "@/components/ui/checkbox"
+import { Label } from "@/components/ui/label"
 import Image from "next/image"
 
 export default function Home() {
   return (
     <div>
-      <h1>hello</h1>
+      <div className="flex items-center gap-3">
+        <Checkbox id="terms" className="cursor-pointer" />
+        <Label htmlFor="terms" className="cursor-pointer">
+          Accept terms and conditions
+        </Label>
+      </div>
     </div>
   )
 }
