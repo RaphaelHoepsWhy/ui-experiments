1111cbaec6b68e77e100411639703f86fae6989d
RaphaelHoepsWhy <raphael.hoeps@why.de>
2025-07-15 14:11:19 +0200

    init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,31 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:tailwindcss/recommended",
+  ),
+  {
+    settings: {
+      tailwindcss: {
+        callees: ["classnames", "cn", "cva"],
+      },
+    },
+    rules: {
+      "tailwindcss/no-custom-classname": ["warn", { whitelist: [] }],
+      "no-console": ["warn", { allow: ["error"] }],
+    },
+  },
+]
 
-export default eslintConfig;
+export default eslintConfig
