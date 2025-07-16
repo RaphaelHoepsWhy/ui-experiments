90abb9f8464423d280490233b85f81314fb8cd5b
RaphaelHoepsWhy <raphael.hoeps@why.de>
2025-07-16 09:46:19 +0200

    feat: checkbox

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 import CheckboxWithLabel from "@/components/CheckboxWithLabel/CheckboxWithLabel"
-import { Label } from "@/components/ui/label"
 
 export default function Home() {
   return (
-    <div>
-      <div className="flex items-center gap-2">
-        <CheckboxWithLabel />
-      </div>
+    <div className="flex min-h-[50vh] w-full items-center justify-center">
+      <CheckboxWithLabel />
     </div>
   )
 }
